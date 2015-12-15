270324de83a54894416f8fdf95e73ba1d21bc1a6
musicbender <musicbender@gmail.com>
2015-12-14 22:06:51 -0800

    Cleaned code and addded keypress playing

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 $(document).ready(function(){
     
-    var context = new (window.AudioContext || window.webkitAudioContext)();
-    var oscillator;
-    
+    var context = new (window.AudioContext || window.webkitAudioContext)(),
+        oscillator,
+        trig = $('.trigger');
     
     $('.play').on('click', function(){
-       
-        var trig = $('.trigger');
-
-        trig.toggleClass('stop');
-        trig.toggleClass('play');
+        playToggle();
+    });
+    
+    $(document).keypress(function(){
+        playToggle();
+    });
+    
+    function playToggle () {
+        trig.toggleClass('stop').toggleClass('play');
         
         if (trig.hasClass('stop')){
             oscillator = context.createOscillator(); // Create sound source
             oscillator.connect(context.destination); // Connect sound to output
-            oscillator.type = 'triangle';
+            oscillator.type = 'sine';
             oscillator.start();
+            trig.text('Stop');
         }
         else {
             oscillator.stop();
+            trig.text('Play');
         }
-    });
-    
-    /*
-    $('.stop').on('click', function(){
-        console.log('stopped');
-        //oscillator.stop();
-        $('.trigger').removeClass('stop');
-        $('.trigger').addClass('play');
-    });*/
-    
-    
+    }
 });
